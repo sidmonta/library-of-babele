9c733d4939cd5974d89a3595474c12c0301ec702
sergio <sergio.berga@hotmail.it>
2022-01-05 19:03:17 +0100

    init refactor book component

--- babele/client/src/components/bookComponents/book/AtomBook.tsx
+++ babele/client/src/components/bookComponents/book/AtomBook.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import { Tools } from '@sidmonta/babelelibrary'
-import * as he from 'he'
-import { useLabel } from '../../../context/labeler'
-import { ThemeComponentFactory } from '../../../context/theme'
-import { generateColorFromString } from '@sidmonta/babelelibrary/build/tools'
+import React from 'react';
+import { Tools } from '@sidmonta/babelelibrary';
+import * as he from 'he';
+import { useLabel } from '../../../context/labeler';
+import { ThemeComponentFactory } from '../../../context/theme';
+import { generateColorFromString } from '@sidmonta/babelelibrary/build/tools';
 
+import { Container, Book, Front, FrontCover, LeftSide, LeftSideContent } from './AtomBookStyle';
 
 const Fallback = ({ url }: { url: string }) => (
-  <div style={{ width: '150px', height: '200px', backgroundColor: Tools.generateColorFromString(url) }}>{url}</div>
-)
+  <div style={{ width: '150px', height: '200px', backgroundColor: Tools.generateColorFromString(url) }}>
+    {url}
+  </div>
+);
 
 export default function AtomBook({ url }: { url: string }) {
-  const label: string = useLabel(url)[0] as string
-  const BookCover = ThemeComponentFactory<{ color: string }>('bookComponents/book/AtomBook', <Fallback url={url} />)
+  const label: string = useLabel(url)[0] as string;
+  const BookCover = ThemeComponentFactory<{ color: string }>(
+    'bookComponents/book/AtomBook',
+    <Fallback url={url} />
+  );
 
-  const bookColor = generateColorFromString(url)
+  const bookColor = generateColorFromString(url);
 
   return (
-    <div className="wid100" color={bookColor}>
-      <BookCover color={bookColor}>{he.decode(label)}</BookCover>
+    <div color={bookColor}>
+      <Container>
+        <Book>
+          <Front>
+            <FrontCover color={bookColor}>{he.decode(label)}</FrontCover>
+          </Front>
+          <LeftSide color={bookColor}>
+            <LeftSideContent>{he.decode(label)}</LeftSideContent>
+          </LeftSide>
+        </Book>
+      </Container>
+      {/* <BookCover color={bookColor}>{he.decode(label)}</BookCover> */}
     </div>
-  )
+  );
 }
